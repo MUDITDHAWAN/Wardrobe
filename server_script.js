81772fc1d55f6118bdc2f926133c13420315135c
unknown <abhimanyu.in@gmail.com>
2019-03-05 15:34:18 +0530

    icons

--- server_script.js
+++ server_script.js
@@ -21,7 +21,7 @@ getConnection(function(err, con){
 // app.use('/css',express.static('css'));
 // app.use('/js',express.static('js'));
 app.use('/images',express.static('images'));
-// app.use('/icons',express.static('css'));
+app.use('/icons',express.static('css'));
 app.use('/',express.static(__dirname));
 app.use(bodyParser.json());
 
